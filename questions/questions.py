6f6f8b28cfe2ed26a5983647a62e7930b5967325
rosalievan <r.vanonzenoort98@gmail.com>
2022-09-29 09:53:41 +0100

    questions wip

--- questions/questions.py
+++ questions/questions.py
@@ -1,6 +1,9 @@
 import nltk
 import sys
 import os
+import string
+from collections import Counter
+import math
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -67,7 +70,13 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
-    raise NotImplementedError
+    new_document = document.translate(str.maketrans('', '', string.punctuation))
+    list = nltk.word_tokenize(new_document)
+    stop_words = nltk.corpus.stopwords.words("english")
+
+    finallist = [x.lower() for x in list if x not in stop_words]
+
+    return finallist
 
 
 def compute_idfs(documents):
@@ -78,8 +87,19 @@ def compute_idfs(documents):
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
     """
-    raise NotImplementedError
+    # Recall that the inverse document frequency of a word is defined by taking the natural logarithm of the number of documents divided by the number of documents in which the word appears.
+    word_list = []
+    return_dict = {}
 
+    for document in documents:
+        word_list.append(list(set(documents[document])))
+    
+    counter = Counter(word_list[0])
+
+    for word in counter:
+        return_dict[word] = math.log(len(documents)) / counter[word]
+
+    return return_dict
 
 def top_files(query, files, idfs, n):
     """
@@ -88,8 +108,18 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
-    raise NotImplementedError
+#     Files should be ranked according to the sum of tf-idf values for any word in the query that also appears in the file. Words in the query that do not appear in the file should not contribute to the file’s score.
+# Recall that tf-idf for a term is computed by multiplying the number of times the term appears in the document by the IDF value for that term.
+    score_dict = {}
+    for file in files:
+        total_score = 0
+        for word in query:
+            total_score += file.count(word)* idfs[word]
+        score_dict[file] = total_score
+    
+    sorted_list = [k for k in sorted(score_dict.items(), key=lambda item: item[1])]
 
+    return sorted_list[:n]
 
 def top_sentences(query, sentences, idfs, n):
     """
@@ -99,7 +129,16 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
-    raise NotImplementedError
+    score_dict = {}
+    for sentence in sentences:
+        total_score = 0
+        for word in query:
+            total_score += sentence.count(word)* idfs[word]
+        score_dict[sentence] = total_score
+    
+    sorted_list = [k for k in sorted(score_dict.items(), key=lambda item: item[1])]
+
+    return sorted_list[:n]
 
 
 if __name__ == "__main__":
